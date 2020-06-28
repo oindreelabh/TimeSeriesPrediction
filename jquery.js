b640128180d3c049841a9d3f05c03949109f6d0a
jainanshul3863 <45658615+jainanshul3863@users.noreply.github.com>
2020-06-29 03:20:01 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,24 @@
+<script>
+      $(document).ready(function() {
+        $('#clientName').change(function() {
+
+          var cl = $('#clientName').val();
+
+          // Make Ajax Request and expect JSON-encoded data
+          $.getJSON(
+            '/get_food' + '/' + cl,
+            function(data) {
+
+              // Remove old options
+              $('#Legal').find('option').remove();                                
+
+              // Add new items
+              $.each(data, function(key, val) {
+                var option_item = '<option value="' + val + '">' + val + '</option>'
+                $('#Legal').append(option_item);
+              });
+            }
+          );
+        });
+      });
+    </script>
