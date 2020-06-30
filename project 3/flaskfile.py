163483d5917a906b8a5d22afd48a53ae094a2544
jainanshul3863 <45658615+jainanshul3863@users.noreply.github.com>
2020-06-30 16:08:51 +0530

    Add files via upload

--- project 3/flaskfile.py
+++ project 3/flaskfile.py
@@ -1,72 +1,75 @@
 
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 import pickle
 import plotly.express as px
 import plotly.io as pio
 from datetime import date
 import datetime
 from dateutil.relativedelta import relativedelta
-from model import model_call
+from model import model_call , predict_top_clients
 import pandas as pd
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import datetime
 import numpy as np
-from helper_functions import give_last_date, take_fields, give_dates
-import predictTopN_clients 
-
+from helper_functions import give_last_date, take_fields, give_dates, give_clients_and_entities
+from forms import OutputForm
+import csv
+from successfulTransactionCompare import comparareClientsTransaction
+from dict import get_dict
+from file1 import get_Ans
+from compf import plot_com, plot_pred
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+allClients=[]
+allLegalEntities=[]
+allData=give_clients_and_entities()
+allClients=allData[0]
+allLegalEntities=allData[1]
+listofatt=take_fields()
+final_result = predict_top_clients(len(allClients))
+
+d = get_dict()
+
+print(final_result)
+print('..................')
+print(allLegalEntities)
+print(',..............................................start......................................................')
+
+
+@app.route('/get_food/<cl>')
+def get_food(cl):
+    return jsonify(d[cl])
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-	listofatt=take_fields()
-	return render_template('index.html',listofatt=listofatt)
+	form = OutputForm()
+	return render_template('index.html', form = form)
 
 
 @app.route("/compare")
 def compare():
-	listofatt=take_fields()
-	return render_template('compare.html', listofatt=listofatt)
+	return render_template('compare.html', listofatt=listofatt,allClients=allClients, allLegalEntities=allLegalEntities)
 
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	cname = request.form['cname'];
-	lename = request.form['lename'];
+	form = OutputForm()
 	from_d = request.form['from'];
-	attribute_value = request.form['attribute_value'];
-	
-	df = model_call(str(cname),str(lename))
-	model = pickle.load(open('model.pkl', 'rb'))
-	lastdate_=give_last_date(cname, lename)
-	pred=model.forecast(6)
-	start = from_d
-	show_predict=np.array(df[str(datetime.datetime.strptime(str(lastdate_),'%Y%m%d').date())])
-	show_predict=np.append(show_predict, pred)
-	
-
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x = df[start:].index, y=df[start:], mode='lines', name='Recorded'))
-	fig.add_trace(go.Scatter(x=give_dates(lastdate_), y=show_predict, mode='lines', name='Predicted'))
-	positive_verdict='This is the graph for predicted values of 6 months from now. It is beneficial to work with this client'
-	negative_verdict='This is the graph for predicted values of 6 months from now. It is not beneficial to work with this client'
-	slope = pred[-1] - pred[0]
-	final_verdict=''
-	if slope>0:
-		final_verdict="Beneficial to work with this client"
-	else :
-		final_verdict="Not beneficial to work with this client"
-	fig.update_layout(title_text=final_verdict)
-	pio.write_html(fig, file='C:/Users/User/Desktop/project 3/templates/predict.html', auto_open=False)
+	cname = form.clientName.data
+	lename = form.Legal.data;
+	#attribute_value = request.form['attribute_value'];
+	plot_pred(cname,lename,from_d)
 	return render_template('predict.html')
-	   
 
 
-      
+
+
 @app.route("/script", methods = ["POST"])
 def script():
 	client1 = request.form['client1'];
@@ -74,54 +77,36 @@ def script():
 	legal1 = request.form['legal1'];
 	legal2 = request.form['legal2'];
 	from_d = request.form['from'];
-	attribute_value = request.form['attribute_value'];
-	start = from_d
-
-	df1 = model_call(str(client1),str(legal1))
-	model = pickle.load(open('model.pkl', 'rb'))
-	lastdate_1=give_last_date(client1, legal1)
-	pred1=model.forecast(6)
-	show_predict1=np.array(df1[str(datetime.datetime.strptime(str(lastdate_1),'%Y%m%d').date())])
-	show_predict1=np.append(show_predict1, pred1)
-
-
-	df2 = model_call(str(client2),str(legal2))
-	model = pickle.load(open('model.pkl', 'rb'))
-	lastdate_2=give_last_date(client2, legal2)
-	pred2=model.forecast(6)
-	show_predict2=np.array(df2[str(datetime.datetime.strptime(str(lastdate_2),'%Y%m%d').date())])
-	show_predict2=np.append(show_predict2, pred2)
-	
-	print('hello')
-	print(start)
-	fig = make_subplots(rows=1, cols=2)
-	fig.add_trace(go.Scatter(x =df1[start:].index,y=df1[start:],mode='lines',name='Recorded trend 1'),row=1,col=1)
-	fig.add_trace(go.Scatter(x=give_dates(lastdate_1),y=show_predict1,mode='lines',name='Predicted trend 1'),row=1,col=1)
-	fig.add_trace(go.Scatter(x = df2[start:].index, y=df2[start:], mode='lines', name='Recorded Trend 2'),row=1,col=2)
-	fig.add_trace(go.Scatter(x=give_dates(lastdate_2), y=show_predict2, mode='lines', name='Predicted trend 2'),row=1,col=2)
-	print('hello2') 
-    
-
-	pio.write_html(fig, file='C:/Users/User/Desktop/project 3/templates/output.html', auto_open=False)
+	#attribute_value = request.form['attribute_value'];
+	plot_com(client1,client2,legal1,legal2,from_d)
 	return render_template('output.html')
+
+
+@app.route("/topNClients.html",  methods = ["POST", "GET"])
+def topNClients() :
+
+	if request.method == 'POST' :
+		number = request.form['number']
+		criteria= request.form['criteria']
+
+		if criteria == 'Mean' :
+			get_Ans(number,final_result)
+
+		else :
+			comparareClientsTransaction(number, allClients)
+
+
+		return render_template('topNClientsGraph.html')
+
+
+	return render_template('topNClients.html', allClientsNumber=len(allClients))
+
 
 @app.route("/alter.html")
 def alter() :
 	return render_template('alter.html')
 
 
-@app.route("/predict_top", methods = ["POST", "GET"])
-def predict_top():
-    lis = {}
-    if request.method == 'POST':
-    	num = request.form['number']
-    	print(num)
-    	lis = predictTopN_clients.predict(int(num))
-    	return render_template('predictTop.html', result = lis)
-    return render_template('predictTop.html', result = lis)	
-
-	
-	
 
 if __name__ == '__main__':
-      app.run(debug=True)    
+      app.run(debug=True)
--- project 3/helper_functions.py
+++ project 3/helper_functions.py
@@ -5,12 +5,13 @@ import datetime
 from dateutil.relativedelta import relativedelta
 import calendar
 
-def give_dates(lastdate_) :
+
+def give_dates(lastdate_ , num=7) :
 	lastdate_=datetime.datetime.strptime(str(lastdate_),'%Y%m%d').date()
 	start_date=lastdate_
 	data = []
 	data.append(start_date)
-	for i in range(7):
+	for i in range(num):
 		days_in_month = calendar.monthrange(start_date.year, start_date.month)[1]
 		dt = start_date + timedelta(days=days_in_month)
 		start_date = dt
@@ -18,7 +19,7 @@ def give_dates(lastdate_) :
 	return data
 
 def give_last_date(cname, lename):
-	with open("C:/Users/User/Desktop/project 3/processed_data.csv", 'r') as csvfile:
+	with open("processed_data.csv", 'r') as csvfile:
 		csvreader = csv.reader(csvfile)
 		fields=next(csvreader)
 		last_date =""
@@ -32,9 +33,26 @@ def give_last_date(cname, lename):
 	return lastdate_
 
 
+def give_clients_and_entities():
+	allData=[]
+	allClients=set()
+	allLegalEntities=set()
+	with open("processed_data.csv", 'r') as csvfile :
+		csvreader = csv.reader(csvfile)
+		fields=next(csvreader)
+		for row in csvreader:
+			allClients.add(row[1])
+			allLegalEntities.add(row[2])
+	allData.append(list(allClients))
+	allData.append(list(allLegalEntities))
+	return allData
+
+
+
 def take_fields():
-	f = open("C:/Users/User/Desktop/project 3/fields_to_take.txt", "r")
+	f = open("fields_to_take.txt", "r")
 	listofatt=[]
 	for x in f:
 		listofatt.append(re.sub('\n', '', x))
 	return listofatt
+
